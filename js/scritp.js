ac39ebbf0877d02d96491adbb523be4075beca23
camilaleutz <leutz1989@gmail.com>
2023-09-14 05:57:28 -0300

    varias funções novas, entre positions, cores, e criando foods

--- js/scritp.js
+++ js/scritp.js
@@ -4,12 +4,44 @@ const ctx = canvas.getContext("2d")
 const size = 30 //tamanho padrão dos quadrados da cobrinha
 
 const snake = [ // array com os comandos das posições da cobrinha
-    { x: 200, y: 200 },
-    { x: 230, y: 200 }
-
+    { x: 270, y: 240 }
 ]
+randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
 
-let direction = ""
+randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size) // gerando sempre no tamanho do canvas.width
+    return Math.round(number / 30) * 30 // aqui o round esta arredondando o number e o number que esta dando esta sendo dividido por 30, e desfazendo a divisão multiplicado por 30 então dando numeros multiplos de 30
+}
+
+const randomColor = () => { // criando cores aleatorias
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+const food ={ // a comida vai carregar sua posição e a sua cor
+    x: randomPosition(), // gerando position aleatoria
+    y:randomPosition(),
+    color: randomColor() // gerando cor aleatoria
+}
+
+let direction, loopId
+
+const drawFood = () => {
+
+const { x, y, color } = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 6
+    ctx.fillStyle = food.color
+    ctx.fillRect( x, y, size, size)
+    ctx.shadowBlur = 0
+
+}
 
 const drawSnake = () => { // função responsável por desenhar a cobrinha
     ctx.fillStyle = "#ddd" // cor da cobrinha
@@ -25,9 +57,9 @@ const drawSnake = () => { // função responsável por desenhar a cobrinha
 
 const moveSnake = () => { // função mover a cobrinha
 
-   if(!direction) return
-   
-    const head = snake[snake.length - 1] 
+    if (!direction) return
+
+    const head = snake[snake.length - 1]
 
     if (direction == "right") { //aqui caso ele esteja indo pra direita
         snake.push({ x: head.x + size, y: head.y }) //vamos adc um novo elemento no array e dar a ele uma nova posição
@@ -44,14 +76,56 @@ const moveSnake = () => { // função mover a cobrinha
     snake.shift() //aqui estamos removendo o ultimo  elemento do array
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1 // largura da linha a ser desenhada
+    ctx.strokeStyle = "#191919" //cor da linha 
 
-setInterval(() => {
+    for (let i = 30; i < canvas.width; i += 30) { // aqui minha variavel let é igual ao valor que eu quero a distancia da linha, sendo assim, a cada 30 coordenadas, uma nova linha
+        ctx.beginPath() // inicia sempre do começo, sem que as linhas se cruzes
+        ctx.lineTo(i, 0) // começa no valor da variavel i
+        ctx.lineTo(i, 600)
+        ctx.stroke() // chamando a linha
 
+        ctx.beginPath() // inicia sempre do começo, sem que as linhas se cruzes
+        ctx.lineTo(0, i) // começa no valor da variavel i que agora aqui ele é a posição y
+        ctx.lineTo(600, i) // aqui agora ele esta no posição y
+        ctx.stroke() // chamando a linha
+    }
+
+}
+
+
+
+const gameLoop = () => {
+    clearInterval(loopId)
     ctx.clearRect(0, 0, 600, 600)
+    drawGrid() // chamando a função de desenhar a linha
+    drawFood() // chamando a função de desenhar a comida
+    moveSnake() //chamando a função de mover a cobra
+    drawSnake() // chamando a função de criar a cobra
 
-    moveSnake()
-    drawSnake()
-    
-}, 300)
+    setTimeout(() => {
+        gameLoop()
+    }, 300)
 
+}
+gameLoop()
 
+document.addEventListener("keydown", ({ key }) => {
+
+    if (key == "ArrowRight" && direction != "left") {
+        direction = "right"
+    }
+
+    if (key == "ArrowLeft" && direction != "right") {
+        direction = "left"
+    }
+
+    if (key == "ArrowDown" && direction != "up") {
+        direction = "down"
+    }
+
+    if (key == "ArrowUp" && direction != "down") {
+        direction = "up"
+    }
+})
